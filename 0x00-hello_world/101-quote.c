a41106cfabf42d444e0611697f79e33fb6ae1ae1
Gift <aghaulor.gift@gmail.com>
2023-10-05 13:05:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 /**
  * main - A c program that prints standard error
- * Return: 1
- */
+ * Return 1
+*/
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
