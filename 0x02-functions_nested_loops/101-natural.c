620ccf586837f8c489458303a3d5eb6ed229d570
Gift <aghaulor.gift@gmail.com>
2023-10-11 19:16:13 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,12 +8,12 @@ int main(void)
 	int sum = 0;
 	int i;
 
-	for (i = 1; 1 < 10; 1++)
+	for (i = 1; i < 10; i++)
 	{
 	if (i % 3 == 0 || 1 % 5 == 0)
 	{
 	sum = sum + i;
 	}
 	}
-	printf("%d\n, sum);
+	printf("%d\n", sum);
 }
