b3cc6ebcb03d01ed5dc90fa613d0a5594321cfb7
Gift <aghaulor.gift@gmail.com>
2023-10-18 05:45:22 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat -  function that appends strings and  adds a terminating null byte.
+ * @dest:the destination value.
+ * @src:the source value.
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	while (*dest != '\0')
+	dest++;
+	while (*src != '\0')
+	*dest++ = *src++;
+	src++;
+	*dest = '\0';
+	return (dest);
+}
