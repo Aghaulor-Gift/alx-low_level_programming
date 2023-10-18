658356244f4fcae65e027eb2d5f4dc9d6cad6b4b
Gift <aghaulor.gift@gmail.com>
2023-10-18 14:44:46 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,8 +14,11 @@ char *_strcat(char *dest, char *src)
 	while (*dest != '\0')
 	dest++;
 	while (*src != '\0')
-	*dest++ = *src++;
-	src++;
+	{
+	*dest = *src;
+        dest++;
+        src++;
+	}
 	*dest = '\0';
 	return (dest);
 }
