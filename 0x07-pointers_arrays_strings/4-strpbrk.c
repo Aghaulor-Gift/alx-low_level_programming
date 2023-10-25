53216edd05250c7e17d222575c00ee2d5fc98390
Gift <aghaulor.gift@gmail.com>
2023-10-24 17:36:56 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,20 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strstr -  function that locates a substring.
+ * _strpbrk - function that searches a string for any of a set of bytes.
  * @s: string to find
  * @accept: what to find
  * Return: value of the address 0
  */
 char *_strpbrk(char *s, char *accept)
 {
-	char *currentAccept = accept;
+	char *currentAccept;
 
 	if (s == NULL || accept == NULL)
 	return (NULL);
 
 	while (*s != '\0')
 	{
+	currentAccept = accept;
 	while (*currentAccept != '\0')
 	{
 	if (*s == *currentAccept)
