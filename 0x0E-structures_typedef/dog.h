c3a1dd11bbf0ce590c8ca8b7aaee98fea18177be
Gift <aghaulor.gift@gmail.com>
2023-11-06 10:51:26 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A function that prints dog information.
+ * @name:name of the dog
+ * @age:age of the dog
+ * @owner:owner of the dog
+ * Return: 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
