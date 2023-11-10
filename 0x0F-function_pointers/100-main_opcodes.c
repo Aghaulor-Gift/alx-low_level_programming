3e47c251e5616e7ebb965260fd09236a9df77976
Gift <aghaulor.gift@gmail.com>
2023-11-10 12:03:44 -0800

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -4,51 +4,40 @@
 #include <stddef.h>
 #include <string.h>
 
-
-
 /**
- * print_opcodes - Print the opcodes of the main function.
- * @num_bytes: Number of opcodes to print.
- * Return: void
- */
-void print_opcodes(int num_bytes)
-{
-	char *main_ptr = (char *)print_opcodes;
-	int i;
-
-	if (num_bytes < 0)
-	{
-	printf("Error\n");
-	exit(2);
-	}
-
-	for (i = 0; i < num_bytes; i++)
-	{
-	printf("%02x", main_ptr[i]);
-	if (i < num_bytes - 1)
-	printf(" ");
-	}
-	printf("\n");
-}
-
-/**
- * main - Entry point.
- * @argc: The number of arguments.
- * @argv: An array of argument strings.
- * Return: 0 on success, 1 on incorrect number of arguments,
- *         2 on negative number of bytes.
+ * main - Prints the opcodes of its own main function
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: 0 on success, 1 if incorrect number of arguments,
+ *         2 if negative bytes
  */
 int main(int argc, char *argv[])
 {
+	int byt, i;
+
 	if (argc != 2)
 	{
 	printf("Error\n");
 	return (1);
 	}
 
-	int num_bytes = atoi(argv[1]);
+	byt = atoi(argv[1]);
 
-	print_opcodes(num_bytes);
+	if (byt < 0)
+	{
+	printf("Error\n");
+	return (2);
+	}
+
+	for (i = 0; i < byt; i++)
+	{
+	printf("%02x", *((unsigned char *)main + i));
+
+	if (i < byt - 1)
+	printf(" ");
+	else
+	printf("\n");
+	}
 
 	return (0);
 }
