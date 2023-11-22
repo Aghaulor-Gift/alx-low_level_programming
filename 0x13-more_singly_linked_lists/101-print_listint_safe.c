aaf2e0436980203b41618e276772b9a1d19ba2ce
Gift <aghaulor.gift@gmail.com>
2023-11-22 02:24:49 -0800

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,14 +13,14 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *current = head;
 	size_t count = 0;
 
-	while (current != NULL)
+	while (current)
 	{
 	printf("[%p] %d\n", (void *)current, current->n);
 	count++;
 
 	if (current <= current->next)
 	{
-	fprintf(stderr, "Error: infinite loop detected\n");
+	fprintf(stderr, "Error: Linked list has a loop\n");
 	exit(98);
 	}
 
