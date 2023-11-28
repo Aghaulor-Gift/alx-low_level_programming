9f773b5a0c9ce86a45d65538455c10f2c82ccb85
Gift <aghaulor.gift@gmail.com>
2023-11-28 05:59:41 -0800

    Upadated 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,13 +22,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	return (-1);
 
 	appended_byt = write(fd, text_content, strlen(text_content));
-	if (appended_byt == -1)
-	return (-1);
+	if (appended_byt != -1)
+	return (1);
 
 	if (text_content == NULL)
 	return (1);
 
 	close(fd);
 
-	return (1);
+	return (appended_byt);
 }
